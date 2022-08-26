3cb402f928c480847a5efa879854d7b397e6767b
@ ANTU <sajolhowlader08@gmail.com>
2022-08-26 19:47:17 +0600

    add new route

--- Routes/candidateTask.route.js
+++ Routes/candidateTask.route.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const {
   giveCandidateTask,
+  getHrTask,
+  allreadyGiven,
 } = require("../Controllers/candidateTask.controller");
 const VerifyToken = require("../VerifyToken/VerifyToken");
 router.post("/candidateTask", VerifyToken, giveCandidateTask);
+router.get("/AllredyGiven", VerifyToken, allreadyGiven);
+router.get("/getHrTask", VerifyToken, getHrTask);
 module.exports = router;

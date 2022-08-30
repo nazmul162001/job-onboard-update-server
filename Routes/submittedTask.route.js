fe53061220da654b6a79c6455abd4f3cd6f2d267
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-30 20:44:59 +0600

    get single submission api

--- Routes/submittedTask.route.js
+++ Routes/submittedTask.route.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const {
   submitTaskFromCandidate,
-  getSubmittedTask
+  getSubmittedTask,
+  singleSubmission
 } = require("../Controllers/submittedTask.controller");
 
 
 const VerifyToken = require("../VerifyToken/VerifyToken");
 router.post("/submitCandidateTask", VerifyToken, submitTaskFromCandidate);
 router.get('/submittedTask', VerifyToken , getSubmittedTask)
+router.get('/submittedTask/candidate/:applicantId', VerifyToken , singleSubmission)
 module.exports=router
